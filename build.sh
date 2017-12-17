844d6e7c30dbd4720dc92644ef9a8e5805ecd8c1
MZO9400 <hamza.hameed00@yahoo.com>
2017-12-17 10:49:22 +0100

    Build.sh: updates

--- build.sh
+++ build.sh
@@ -7,11 +7,12 @@ AK_DIR=$KERNEL_DIR/../AnyKernel2
 TC_DIR=$KERNEL_DIR/../TC/aarch64-linux-android-4.9
 DATE=$(date +"%Y%m%d")
 VERSION="0.1A"
-KERNELFINAL="Placeholderkernelname_capricorn-"$VERSION"-"$DATE
-
+KERNELFINAL="Panthera_capricorn-"$VERSION"-"$DATE".zip"
+ZIPFILE=$KERNEL_DIR/../$KERNELFINAL
 export ARCH=arm64
 export CROSS_COMPILE=$TC_DIR/bin/aarch64-linux-android-
 export LD_LIBRARY_PATH=$TC_DIR/lib/
+export KBUILD_OUTPUT=$KERNEL_DIR/../out
 
 echo "zip: "$KERNELFINAL", AK: "$AK_DIR", TC: "$TC_DIR, ARCH: $ARCH, CC: $CROSS_COMPILE
 sleep 1
@@ -21,9 +22,22 @@ sleep 1
 make clean
 make mrproper
 make capricorn_defconfig
-make -j$( nproc --all )
+if [ -f ../lastlog ]; then rm ../lastlog; fi
+time make -j$(nproc) | tee -a ../lastlog
 
-mv $KERNEL_DIR/arch/arm64/boot/Image.gz $AK_DIR/zImage
-cd $AK_DIR/capricorn
+if [ -f $KERNEL_DIR/../out/arch/arm64/boot/Image.gz-dtb ]
+   then
+	mv $KERNEL_DIR/../out/arch/arm64/boot/Image.gz-dtb $AK_DIR/zImage
+	cd $AK_DIR
 
-zip -r9 $KERNELFINAL * -x *.zip $KERNELFINAL
+	zip -r9 $KERNELFINAL * -x *.zip $KERNELFINAL
+	mv $KERNELFINAL ..
+	cd $KERNEL_DIR
+	make clean
+	rm -rf ../out
+        if [ -f ../lastlog ]; then rm ../lastlog; fi
+	echo Output file: '$ZIPFILE' = $ZIPFILE
+   else
+
+	echo "Error occured, cannot find zImage, check logs for info"
+fi
